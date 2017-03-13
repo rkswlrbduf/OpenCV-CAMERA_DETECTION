2414c85a238192ccd49c356294746b7b995a8cec
rkswlrbduf <rkswlrbduf@gmail.com>
2017-03-13 16:52:44 +0900

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,29 @@
+#include <jni.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+extern "C" {
+
+    int process(Mat img_input, Mat &img_result)
+    {
+        cvtColor( img_input, img_result, CV_RGBA2GRAY);
+
+        return(0);
+    }
+
+    JNIEXPORT jint JNICALL
+    Java_friend_skplanet_myapplication_MainActivity_convertNativeLib(JNIEnv*, jobject, jlong addrInput, jlong addrResult) {
+
+        Mat &img_input = *(Mat *) addrInput;
+        Mat &img_result = *(Mat *) addrResult;
+
+        int conv = process(img_input, img_result);
+        int ret = (jint) conv;
+
+        return ret;
+    }
+
+}
