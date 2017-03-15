6361f3e3577f508b3cda02fcfae666da569505fb
rkswlrbduf <rkswlrbduf@gmail.com>
2017-03-15 14:54:18 +0900

    주석 처리과정

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,24 +6,42 @@
 using namespace cv;
 
 extern "C" {
-
-    int process(Mat img_input, Mat &img_result)
-    {
-        cvtColor( img_input, img_result, CV_RGBA2GRAY);
-
-        return(0);
-    }
-
     JNIEXPORT jint JNICALL
-    Java_friend_skplanet_myapplication_MainActivity_convertNativeLib(JNIEnv*, jobject, jlong addrInput, jlong addrResult) {
+    Java_friend_skplanet_myapplication_MainActivity_convertNativeLib(JNIEnv* env, jobject thiz, jint width, jint height, jbyteArray yuv, jintArray bgra)
+    {
+        jbyte* _yuv  = env->GetByteArrayElements(yuv, 0);
+        jint*  _bgra = env->GetIntArrayElements(bgra, 0);
 
-        Mat &img_input = *(Mat *) addrInput;
-        Mat &img_result = *(Mat *) addrResult;
+        Mat myuv(height + height/2, width, CV_8UC1, (unsigned char *)_yuv);
+        Mat mbgra(height, width, CV_8UC4, (unsigned char *)_bgra);
+        //_bgra int형 배열로부터 8bit 4채널로 바꿔 mbgra라는 Mat 구조체
+        Mat mgray(height, width, CV_8UC1, (unsigned char *)_yuv);
+        //_bgra int형 배열로부터 8bit 1채널로 바꿔 mbgra라는 Mat 구조체
+        //Please make attention about BGRA byte order
+        //ARGB stored in java as int array becomes BGRA at native level
+        cvtColor(myuv, mbgra, CV_YUV420sp2BGR, 4); //YUV로 들어온 색상체계를 BGR로 바꾼다.
+        //일부 CV_YUV420sp2BGR이 안먹힌다. sp를 i로 바꿔서 해도 무방할듯..?
+        vector<KeyPoint> v;
+        FastFeatureDetector detector(50); //특징점 추출 함수 호출
+        detector.detect(mgray, v);  //특징점 추출 함수 호출
+        for( size_t i = 0; i < v.size(); i++ ) //특징점이 저장된 v 벡터를 중점으로 CIRCLE을 그려준다.
+            circle(mbgra, Point(v[i].pt.x, v[i].pt.y), 10, Scalar(0,0,255,255));
 
-        int conv = process(img_input, img_result);
-        int ret = (jint) conv;
 
-        return ret;
+//픽셀데이터 접근 추가사항(본인이 추가한 내용이다.)
+        for(int co=0; co<mbgra.cols; co++)
+        {
+            for(int ro=0; ro<mbgra.rows; ro++)
+            {
+                mbgra.at<Vec4b>(ro,co)[0] = 255; // 블루(Blue)값을 255로 바꿔준다.
+                // mbgra.at<Vec4b>(ro,co)[1] = 255; //이것들까지 바꾸면 하얀색 화면을 보게될것이다.
+                // mbgra.at<Vec4b>(ro,co)[2] = 255;
+            }
+        }
+
+
+//메모리 해제
+        env->ReleaseIntArrayElements(bgra, _bgra, 0);
+        env->ReleaseByteArrayElements(yuv, _yuv, 0);
     }
-
 }
--- app/src/main/java/friend/skplanet/myapplication/MainActivity.java
+++ app/src/main/java/friend/skplanet/myapplication/MainActivity.java
@@ -27,52 +27,50 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     ImageView DrawArea;
 
-    private Mat img_input;
-    private Mat img_result;
-    private static final String TAG = "opencv";
-    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat img_input; //Image Input Memory Analyzer tools
+    private Mat img_result; //Image Result Memory Analyzer tools
+    private static final String TAG = "opencv"; // TAG = opencv
+    private CameraBridgeViewBase mOpenCvCameraView; // Define CameraBridgeViewBase
 
-    public native int convertNativeLib(long matAddrInput, long matAddrResult);
+    public native int convertNativeLib(long matAddrInput, long matAddrResult); // native-lib 에서 사용되는 함수이름과 매개변수 long형 matAddrInput과 long형 matAddrResult
 
-    static final int PERMISSION_REQUEST_CODE = 1;
-    String[] PERMISSIONS  = {"android.permission.CAMERA"};
+    static final int PERMISSION_REQUEST_CODE = 1; // set PERMISSION_REQUEST_CODE to 1
+    String[] PERMISSIONS  = {"android.permission.CAMERA"}; // PERMISSIONS = android.permission.CAMERA
 
+    // 라이브러리 로드
     static {
-        System.loadLibrary("opencv_java3");
-        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3"); // opencv_java3 라이브러리 로드
+        System.loadLibrary("native-lib"); // native-lib 라이브러리 로드
     }
 
+    // String을 입력받아서 퍼미션 허가 여부 확인(퍼미션 허가시 True, 퍼미션 미허가시 False 반환)
     private boolean hasPermissions(String[] permissions) {
         int ret = 0;
-        //스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
-        for (String perms : permissions){
+
+        for (String perms : permissions){ // 스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
             ret = checkCallingOrSelfPermission(perms);
-            if (!(ret == PackageManager.PERMISSION_GRANTED)){
-                //퍼미션 허가 안된 경우
+            if (!(ret == PackageManager.PERMISSION_GRANTED)){ // 퍼미션 허가 안된 경우
                 return false;
             }
-
         }
-        //모든 퍼미션이 허가된 경우
-        return true;
+        return true; // 모든 퍼미션이 허가된 경우
     }
 
+    // 마시멜로( API 23 )이상에서 런타임 퍼미션(Runtime Permission) 요청 onCreate 실행시 시작됨
     private void requestNecessaryPermissions(String[] permissions) {
-        //마시멜로( API 23 )이상에서 런타임 퍼미션(Runtime Permission) 요청
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE); // PERMISSION_REQUEST_CODE로 퍼미션 요청함
         }
     }
 
-
-
+    // BaseLoaderCallback을 불러서 opencv 초기 세팅
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
                 {
-                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.enableView(); // mOpenCvCameraView Enable 상태로 전환
                 } break;
                 default:
                 {
@@ -82,6 +80,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         }
     };
 
+    // 퍼미션 허가 Result 과정( 퍼미션 다이얼로그 팝업 )
     @Override
     public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
         switch(permsRequestCode){
@@ -94,7 +93,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
                         if (!camreaAccepted  )
                         {
-                            showDialogforPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+                            showDialogforPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다."); // 다이얼로그를 띄움
                             return;
                         }else
                         {
@@ -106,6 +105,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         }
     }
 
+    // 퍼미션 다이얼로그
     private void showDialogforPermission(String msg) {
 
         final AlertDialog.Builder myDialog = new AlertDialog.Builder(  MainActivity.this);
@@ -135,7 +135,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
         DrawArea = (ImageView) findViewById(R.id.imageview);
 
-        getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // 화면 꺼짐 방지기능 활성화
 
         if (!hasPermissions(PERMISSIONS)) { //퍼미션 허가를 했었는지 여부를 확인
             requestNecessaryPermissions(PERMISSIONS);//퍼미션 허가안되어 있다면 사용자에게 요청
@@ -144,34 +144,35 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         }
 
         mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE); //SurfaceView의 일반 View 와 다른점은 일반 View는 한번 표출되면 생성된 모습 그대로이지만 SurfaceView는 화면의 변화가 실시간이다.
         mOpenCvCameraView.setCvCameraViewListener(this);
         mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
-        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS); // openCV 시작
     }
 
+    // Pause 동작
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
+    // Resume 동작
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
 
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "onResume :: Internal OpenCV library not found.");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback); // 매개변수 Version, AppContext, Callback
         } else {
             Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS); // openCV 시작
         }
     }
 
+    // Destroy 동작
     public void onDestroy() {
         super.onDestroy();
 
@@ -179,20 +180,23 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
             mOpenCvCameraView.disableView();
     }
 
+    // enableView 다음에 실행된다. camera preview 시작시 실행 ( 매개변수 width, height -> Frame의 width, height )
     @Override
     public void onCameraViewStarted(int width, int height) {
 
     }
 
+    // camera preview 정지시 실행된다.
     @Override
     public void onCameraViewStopped() {
 
     }
 
+    // 아래 함수를 거친 프레임이 출력된다.
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
-        if (inputFrame.rgba() != null)
+        if (inputFrame.rgba() != null) // inputFrame 포맷 형식이 rgba인지 확인한다.
         {
             img_input = inputFrame.rgba();
 
